7f6daaac5de4fadc8723a8877532288dc2239969
Rob Head <robert.head@gmail.com>
2026-04-20 16:48:17 -0700

    Revise story to list plants in zones
    - instead of pinning exact locations.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -6,13 +6,13 @@ export const Route = createRootRoute({
       <header className="app-header">
         <div className="app-header__inner">
           <h1 className="app-header__wordmark">
-            <Link to="/" className="app-header__link">
+            <Link to="/" search={{ address: '' }} className="app-header__link">
               <span className="app-header__fire">Fire</span>
               <span className="app-header__shire">Shire</span>
             </Link>
           </h1>
           <nav className="app-nav">
-            <Link to="/" className="app-nav__link" activeProps={{ className: 'app-nav__link app-nav__link--active' }}>
+            <Link to="/" search={{ address: '' }} className="app-nav__link" activeProps={{ className: 'app-nav__link app-nav__link--active' }}>
               Map
             </Link>
             <Link to="/prepare" className="app-nav__link" activeProps={{ className: 'app-nav__link app-nav__link--active' }}>
